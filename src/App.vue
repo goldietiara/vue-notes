a177b6ba14f1ea8820f39f15acb528a8befcaf43
owy <126146587+goldietiara@users.noreply.github.com>
2023-10-10 14:49:00 +0700

    update styling
    update all ui/ux

--- src/App.vue
+++ src/App.vue
@@ -8,17 +8,22 @@
       <header
         class="flex w-full px-40 py-3 justify-between font-semibold text-4xl border-b-[1px] mt-5"
       >
-        <h1>Notess</h1>
+        <h1>Notes</h1>
         <button
-          class="rounded-full bg-yellow-300 w-12 h-12 self-center text-center inline-block align-middle transition-all ease-in-out duration-100 active:text-3xl active:bg-black active:text-yellow-300 hover:bg-black hover:text-yellow-300"
+          class="rounded-xl w-12 h-12 self-center text-center inline-block align-middle transition-all ease-in-out duration-100 active:text-3xl active:bg-black/5 active:text-black hover:bg-black/5 hover:text-black"
         >
           +
         </button>
       </header>
       <div class="flex px-40">
         <div
-          class="flex flex-col justify-between bg-pink-200 px-4 py-3 rounded-md w-[250px] h-full min-h-[200px] gap-5"
+          class="group relative flex flex-col justify-between bg-pink-200 px-4 py-2 rounded-md w-[250px] h-full min-h-[200px] gap-5 hover:cursor-pointer hover:outline hover:outline-4 outline-black/10 outline-offset-4"
         >
+          <div
+            class="absolute invisible right-2 top-2 h-6 w-6 bg-black/70 rounded-full text-center text-sm text-white/90 group-hover:visible group-active:visible"
+          >
+            x
+          </div>
           <p>henlo</p>
           <p class="text-black/50">date</p>
         </div>
